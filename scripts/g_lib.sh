8aab968fd50042504b5fa123b882b92a1d608dae
Geraint Edwards <gedgey@gmail.com>
2018-01-08 15:28:01 +0000

    use g_trace (deprecate g_info2)

--- scripts/g_lib.sh
+++ scripts/g_lib.sh
@@ -12,7 +12,8 @@ g_colr() { local col=$1 recurse= rst=$'\e[0m'; shift  # '[-r] bright_white_on_re
 }
 g_ts()      { echo  $(g_colr    BLUE   $(date '+%F %T')) ${g_ts_host:+$(g_colr green $g_ts_host)} "$@"; }
 g_info()    { g_ts "$(g_colr -r cyan   "$@")"; }
-g_info2()   { g_info "$@" >&2; }
+g_trace()   { g_info "$@" >&2; }
+g_info2()   { g_info "$@" >&2; } # DEPRECATED
 g_err()     { g_ts "$(g_colr -r RED    ERROR "$@")" >&2; }
 g_warn()    { g_ts "$(g_colr -r YELLOW WARN  "$@")" >&2; }
 g_log()     { g_ts "$@"; }
